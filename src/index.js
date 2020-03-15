86bfef1ece54b314ff084350af6c268da3c3316d
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2020-03-15 15:24:17 +0300

    feat: morse done

--- src/index.js
+++ src/index.js
@@ -35,10 +35,69 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+let k = 0;
+    let array = expr.split('');
+    let arrraDivide10 = [];
+    let arrraDivide2 = [];
+    let str = '';
+    array.map(function(item, index) {
+        if (index % 2 === 0 & index>1) {
+          str = '';
+          str=str.concat(item);
+          k=k+1;
+          arrraDivide2[k]=str;
+        }
+        else {
+          str=str.concat(item);
+          arrraDivide2[k]=str;
+        }
+      });
+arrraDivide2.map(function(item, index) {
+        if (item==10) {
+          arrraDivide2[index]='.';
+        }
+        else if (item==11) {
+          arrraDivide2[index]='-';
+        }
+        else if (item==00) {
+          arrraDivide2[index]='';
+        }
+        else if (item=='**') {
+          arrraDivide2[index]='**';
+        }
+      });
+
+let morseArr = [];
+str = '';
+k=0;
+for (let i = 0; i<arrraDivide2.length; i++) {
+  if (i % 5 === 0 & i>4) {
+
+    k=k+1;
+        morseArr[k]=str;
+    str='';
+    str=str.concat(arrraDivide2[i]);
+
+  }
+  else {
+  str=str.concat(arrraDivide2[i]);
+  morseArr[k]=str;
+  }
+}
+
+str = '';
+
+morseArr.map(function(item, index) {
+
+let elem = '';
+elem = morseArr[index];
+str = str.concat(MORSE_TABLE[elem]);
+});
+return(str);
 }
 
 module.exports = {
